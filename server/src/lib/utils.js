655c8e627d88c8a698a987302d9d993656689b3a
Himanshu <himanshu404mishra@gmail.com>
2025-10-04 18:11:15 +0545

    hi i am tired

--- server/src/lib/utils.js
+++ server/src/lib/utils.js
@@ -1,16 +1,16 @@
-import jwt from "jsonwebtoken"
+import jwt from "jsonwebtoken";
 
 export const generateToken = function (adminId, res) {
+  const token = jwt.sign({ adminId }, process.env.JWT_SECRET, {
+    expiresIn: "7d",
+  });
+  res.cookie("jwt", token, {
+    maxAge: 7 * 24 * 60 * 60 * 1000, // 7d -> MS
+    httpOnly: true, // prevents attacks cross-site scripting attack
+    sameSite: "none", // CSRF attacks corss-site request forgery attacks
+    secure: process.env.NODE_ENV !== "development",
+    path: "/",
+  });
 
-    const token = jwt.sign({adminId},process.env.JWT_SECRET, {
-        expiresIn:"7d"
-    })
-    res.cookie("jwt",token,{
-        maxAge: 7 *24 *60 * 60 *1000, // 7d -> MS
-        httpOnly: true, // prevents attacks cross-site scripting attack
-        sameSite: "strict", // CSRF attacks corss-site request forgery attacks
-        secure: process.env.NODE_ENV !== "development"
-    })
-
-    return token
-}
+  return token;
+};
