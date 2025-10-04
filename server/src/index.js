96711ab54eb08a8f09dcd49d716d5957511632db
Himanshu <himanshu404mishra@gmail.com>
2025-10-04 16:52:15 +0545

    tired

--- server/src/index.js
+++ server/src/index.js
@@ -27,7 +27,7 @@ app.use(cookieParser());
 // for CORS
 app.use(
     cors({
-        origin: "http://localhost:5173",
+        origin: ["http://localhost:5173","https://career-page-21ju.vercel.app"],
         credentials: true,
     })
 );
